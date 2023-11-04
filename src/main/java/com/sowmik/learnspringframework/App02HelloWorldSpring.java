a9e0a3cefde3b6fab9a9a1d6fcc8fb01cca899d4
sowmik-sec <sowmik.sec@gmail.com>
2023-11-04 09:43:40 +0600

    creating more java spring beans in spring java configuration file

--- src/main/java/com/sowmik/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/sowmik/learnspringframework/App02HelloWorldSpring.java
@@ -12,5 +12,8 @@ public class App02HelloWorldSpring {
         // name -@Bean
         //3: Retrieving Beans managed by Spring
         System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("address"));
     }
 }
--- src/main/java/com/sowmik/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/sowmik/learnspringframework/HelloWorldConfiguration.java
@@ -3,11 +3,34 @@ package com.sowmik.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// Eliminate verbosity in creating Java Beans
+// Public accessor methods, constructor,
+// equals, hashCode and toString are automatically created.
+// Released in JDK 16.
+
+record Person (String name, int age) { };
+
+record Address (String firstLine, String city) { };
+
 @Configuration
 public class HelloWorldConfiguration {
 
     @Bean
     public String name() {
         return "Sowmik";
+    }
+
+    @Bean
+    public int age() {
+        return 16;
+    }
+
+    @Bean
+    public Person person() {
+        return new Person("Habib", 15);
+    }
+
+    @Bean Address address() {
+        return new Address("Bazar Road", "Bhurungamari");
     }
 }
