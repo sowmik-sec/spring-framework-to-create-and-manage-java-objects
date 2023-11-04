7c5af933972fb2aa862a190874a61aaf0c5d6de8
sowmik-sec <sowmik.sec@gmail.com>
2023-11-04 13:10:25 +0600

    exploring spring framework bean auto wiring primary and qualifier

--- src/main/java/com/sowmik/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/sowmik/learnspringframework/App02HelloWorldSpring.java
@@ -3,6 +3,8 @@ package com.sowmik.learnspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class App02HelloWorldSpring {
     public static void main(String[] args) {
         //1: Launch a Spring Context
@@ -15,8 +17,13 @@ public class App02HelloWorldSpring {
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
         System.out.println(context.getBean("address2"));
-        //System.out.println(context.getBean(Address.class));
+        System.out.println(context.getBean(Person.class));
+        System.out.println(context.getBean(Address.class));
         System.out.println(context.getBean("person2MethodCall"));
         System.out.println(context.getBean("person3Parameters"));
+        System.out.println(context.getBean("person5Qualifier"));
+
+        // System.out.println
+        //Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }
--- src/main/java/com/sowmik/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/sowmik/learnspringframework/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
 package com.sowmik.learnspringframework;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 // Eliminate verbosity in creating Java Beans
 // Public accessor methods, constructor,
@@ -36,15 +38,28 @@ public class HelloWorldConfiguration {
     }
 
     @Bean
+    @Primary
     public Person person3Parameters(String name, int age, Address address3) { // name, age, address2
         return new Person(name, age, address3);
     }
 
+    @Bean
+    public Person person4Parameters(String name, int age, Address address) { // name, age, address2
+        return new Person(name, age, address);
+    }
+
+    @Bean
+    public Person person5Qualifier(String name, int age, @Qualifier("address3qualifier") Address address) { // name, age, address2
+        return new Person(name, age, address);
+    }
+
     @Bean(name = "address2")
+    @Primary
     Address address() {
         return new Address("Bazar Road", "Bhurungamari");
     }
     @Bean(name = "address3")
+    @Qualifier("address3qualifier")
     Address address3() {
         return new Address("Paglarhat Road", "Bhurungamari");
     }
