1a59eccbf0e13ef1eb5316b81d02d2620e94e959
sowmik-sec <sowmik.sec@gmail.com>
2023-11-06 21:47:58 +0600

    different types of dependency injection

--- src/main/java/com/sowmik/learnspringframework/game/GamingAppLauncherApplication.java
+++ src/main/java/com/sowmik/learnspringframework/game/GamingAppLauncherApplication.java
@@ -1,7 +1,5 @@
-package com.sowmik.learnspringframework;
+package com.sowmik.learnspringframework.game;
 
-import com.sowmik.learnspringframework.game.GameRunner;
-import com.sowmik.learnspringframework.game.GamingConsole;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
