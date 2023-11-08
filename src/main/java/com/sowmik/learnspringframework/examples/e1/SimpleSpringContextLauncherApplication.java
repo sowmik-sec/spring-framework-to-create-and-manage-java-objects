39ffa7faa39897e044e43237baade063bb895dd7
sowmik-sec <sowmik.sec@gmail.com>
2023-11-08 12:07:32 +0600

    exploring java spring framework bean scopes prototype and singleton

---
+++ src/main/java/com/sowmik/learnspringframework/examples/e1/SimpleSpringContextLauncherApplication.java
@@ -0,0 +1,36 @@
+package com.sowmik.learnspringframework.examples.e1;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+class NormalClass {
+
+}
+
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Component
+class PrototypeClass {
+
+}
+
+@Configuration
+@ComponentScan
+public class SimpleSpringContextLauncherApplication {
+    public static void main(String[] args) {
+        try(var context = new AnnotationConfigApplicationContext(SimpleSpringContextLauncherApplication.class)) {
+            System.out.println(context.getBean(NormalClass.class));
+            System.out.println(context.getBean(NormalClass.class));
+
+            System.out.println(context.getBean(PrototypeClass.class));
+            System.out.println(context.getBean(PrototypeClass.class));
+            System.out.println(context.getBean(PrototypeClass.class));
+        }
+    }
+}
