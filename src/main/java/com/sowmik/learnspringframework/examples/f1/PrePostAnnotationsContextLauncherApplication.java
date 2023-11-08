6fb240f76407b6ccab7446dbfae294e83b3533f4
sowmik-sec <sowmik.sec@gmail.com>
2023-11-08 12:20:05 +0600

    exploring spring beans PostConstruct and PreDestroy

---
+++ src/main/java/com/sowmik/learnspringframework/examples/f1/PrePostAnnotationsContextLauncherApplication.java
@@ -0,0 +1,48 @@
+package com.sowmik.learnspringframework.examples.f1;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+class SomeClass {
+    private SomeDependency someDependency;
+
+    public SomeClass(SomeDependency someDependency) {
+        this.someDependency = someDependency;
+        System.out.println("All dependencies are ready");
+    }
+
+    @PostConstruct
+    public void initialize() {
+        someDependency.getReady();
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        System.out.println("Clean up");
+    }
+}
+
+@Component
+class SomeDependency {
+
+    public void getReady() {
+        System.out.println("Some logic using SomeDependency");
+    }
+}
+
+@Configuration
+@ComponentScan
+public class PrePostAnnotationsContextLauncherApplication {
+    public static void main(String[] args) {
+        try(var context = new AnnotationConfigApplicationContext(PrePostAnnotationsContextLauncherApplication.class)) {
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+        }
+    }
+}
